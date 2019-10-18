5094620d66c115a2cada17288f6e8d5762af2a1a
Jihwan Lee <osjin6633@gmail.com>
2019-10-18 12:21:26 +0900

    Drill 07
    2016182031 이지환 Drill 07
    Commit 2
    
    https://www.youtube.com/watch?v=jsXN9Ohp0HA

--- Drill-07/boy_grass_object.py
+++ Drill-07/boy_grass_object.py
@@ -13,17 +13,50 @@ class Grass:
 
 class Boy:
     def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
+        self.frame = randint(0, 7)
+        self.x, self.y = randint(100, 700), 90
         self.image = load_image('run_animation.png')
 
     def update(self):
-        self.frame = randint(0, 7)
-        self.x, self.y = randint(100, 700), 90
+        self.frame = (self.frame + 1) % 8
         self.x += 5
 
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
+class Big_ball:
+    def __init__(self):
+        self.frame = randint(0, 7)
+        self.x, self.y = randint(1, 799), 599
+        self.falling_rate = randint(1, 20)
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if self.y > 50:
+            self.y -= self.falling_rate
+        else:
+            self.y = self.y
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Small_ball:
+    def __init__(self):
+        self.frame = randint(0, 7)
+        self.x, self.y = randint(1, 799), 599
+        self.falling_rate = randint(10, 20)
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        if self.y > 50:
+            self.y -= self.falling_rate
+        else:
+            self.y = self.y
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 
 def handle_events():
@@ -37,20 +70,32 @@ def handle_events():
 
 # initialization code
 open_canvas()
+small_ball_count = randint(1, 20)
+big_ball_count = 20 - small_ball_count
 boy = Boy()
 grass = Grass()
 running = True
 team = [Boy() for i in range(11)]
+big_ball = [Big_ball() for i in range(big_ball_count)]
+small_ball = [Small_ball() for i in range(small_ball_count)]
 
 # game main loop code
 while running:
     handle_events()
     for boy in team:
         boy.update()
+    for ball in big_ball:
+        ball.update()
+    for ball in small_ball:
+        ball.update()
     clear_canvas()
     grass.draw()
     for boy in team:
         boy.draw()
+    for ball in big_ball:
+        ball.draw()
+    for ball in small_ball:
+        ball.draw()
     update_canvas()
     delay(0.05)
 
